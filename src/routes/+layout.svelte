11e627de8833581251ca78241efc6d8922a8bc7f
Aiden Redmond <adnrdmnd@gmail.com>
2025-08-15 22:27:54 -0400

    spend an hour crafting the perfect gradient

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,23 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<div class="mx-6 mt-3 flex items-center justify-between">
+	<div>
+		<!-- <h1
+			class="bg-[linear-gradient(90deg,rgba(170,140,40,1)_0%,rgba(212,175,55,1)_35%,rgba(255,240,120,1)_50%,rgba(212,175,55,1)_65%,rgba(170,140,40,1)_100%)] bg-clip-text font-serif text-7xl text-transparent italic"
+		>
+			pico
+		</h1> -->
+		<h1
+			class="bg-[linear-gradient(90deg,rgba(120,120,120,1)_0%,#BFC0C0_35%,rgba(230,230,230,1)_50%,#BFC0C0_65%,rgba(120,120,120,1)_100%)] bg-clip-text font-serif text-7xl text-transparent italic"
+		>
+			pico
+		</h1>
+	</div>
+
+	<div class="flex gap-6 text-xl">
+		<a href="/dashboard">dashboard</a>
+		<a href="/transactions">transactions</a>
+	</div>
+</div>
 {@render children?.()}
