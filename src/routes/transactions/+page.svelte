ad24df972ba50f8a5daae8cc97d2b29b370beb03
Aiden Redmond <adnrdmnd@gmail.com>
2025-08-17 14:51:58 -0400

    add some fitlers that don't rlly work

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
 	let { data } = $props();
+
+	const transactions = $state(data.transactions);
+
+	// have a static list of all months, and display 'no rows found'
+	//  if transactions with specified month do not exist within this data
+	const months = [
+		{ name: 'january', number: 1 },
+		{ name: 'feburary', number: 2 },
+		{ name: 'march', number: 3 },
+		{ name: 'april', number: 4 },
+		{ name: 'may', number: 5 },
+		{ name: 'june', number: 6 },
+		{ name: 'july', number: 7 },
+		{ name: 'august', number: 8 },
+		{ name: 'september', number: 9 },
+		{ name: 'october', number: 10 },
+		{ name: 'november', number: 11 },
+		{ name: 'december', number: 12 }
+	];
+
+	// dynamically display the years based on the data we have
+	const yearSet: Set<string> = new Set<string>();
+
+	for (const t of transactions) {
+		yearSet.add(t.date.slice(0, 4));
+	}
+
+	const years = Array.from(yearSet).sort().reverse();
 </script>
 
 {#snippet column(colName: string)}
@@ -7,10 +35,38 @@
 {/snippet}
 
 {#snippet row(value: any, focus: boolean = false)}
-	<td class="border-b-1 border-silver/10 px-6 py-4">{value}</td>
+	<td
+		class={[
+			'border-b border-silver/10 px-6 py-4',
+			{ 'text-silver/85': focus, 'text-silver/50': !focus }
+		]}>{value}</td
+	>
 {/snippet}
 
 <div class="mx-36 mt-12">
+	<div class="mb-4 flex justify-between">
+		<div class="flex gap-4">
+			<input
+				type="text"
+				class="border-1 border-silver/10 px-2 py-2"
+				placeholder="search for a transaction"
+			/>
+			<select name="month" id="month-select" class="border border-silver/10 px-2">
+				<option value="all months">all months</option>
+				{#each months as month}
+					<option value={month.number}>{month.name}</option>
+				{/each}
+			</select>
+			<select name="year" id="year-select" class="border border-silver/10 px-2">
+				<option value="0">all years</option>
+				{#each years as year}
+					<option value={year}>{year}</option>
+				{/each}
+			</select>
+		</div>
+
+		<button class="border border-silver/10 bg-gray/10 px-2 text-silver">add a transaction</button>
+	</div>
 	<table class="w-full border-1 border-silver/10 text-left text-white">
 		<thead class="border-b-1 border-silver/10 bg-gray/10">
 			<tr>
@@ -22,20 +78,15 @@
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				{@render row('8/15/2025')}
-				{@render row('Print frames')}
-				{@render row('Home')}
-				{@render row('Decorations')}
-				{@render row('$180.50')}
-			</tr>
-			<tr>
-				{@render row('8/15/2025')}
-				{@render row('Print frames')}
-				{@render row('Home')}
-				{@render row('Decorations')}
-				{@render row('$180.50')}
-			</tr>
+			{#each transactions as transaction}
+				<tr>
+					{@render row(transaction.date)}
+					{@render row(transaction.description, true)}
+					{@render row(transaction.category_id)}
+					{@render row(transaction.subcategory_id)}
+					{@render row('$' + transaction.amount)}
+				</tr>
+			{/each}
 		</tbody>
 	</table>
 </div>
